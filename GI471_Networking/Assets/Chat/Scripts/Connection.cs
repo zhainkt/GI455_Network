05ae2355236fd57875fe52aa5c419c2f215717b0
Kosit Taneakkhun <kost_t@bu.ac.th>
2021-01-28 21:08:41 +0700

    add project

---
+++ GI471_Networking/Assets/Chat/Scripts/Connection.cs
@@ -0,0 +1,206 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class Connection : MonoBehaviour
+{
+    public enum ConnectionState
+    {
+        Disconnect,
+        Connect
+    }
+
+    public enum ConnectionRole
+    {
+        None,
+        Server,
+        Client
+    }
+
+    private Socket socket;
+    private int byteSize = 32;
+    private byte[] buffer;
+    AsyncCallback callback;
+    EndPoint endPoint;
+
+    public struct ClientData
+    {
+        public Socket socket;
+        public string groupID;
+    }
+
+    private List<Socket> clientSocketList = new List<Socket>();
+
+    private ConnectionState connectionState;
+    private ConnectionRole connectionRole;
+
+    public static Connection instance;
+
+    public delegate void DelegateHandleCallback(string msg);
+
+    public event DelegateHandleCallback OnCreateServerSuccess;
+    public event DelegateHandleCallback OnCreateServerFail;
+
+    public event DelegateHandleCallback OnJoinRoomSuccess;
+    public event DelegateHandleCallback OnJoinRoomFail;
+
+    private WaitForSeconds waitConnecting = new WaitForSeconds(1.0f);
+    private bool isJoiningRoom = false;
+
+    public ConnectionState GetConnectionState()
+    {
+        return connectionState;
+    }
+
+    public ConnectionRole GetConnectionRole()
+    {
+        return connectionRole;
+    }
+
+    public void Awake()
+    {
+        instance = this;
+
+        OnCreateServerSuccess += CallbackCreateServerSuccess;
+    }
+
+    public void StartServer(int port)
+    {
+        buffer = new byte[byteSize];
+        socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        endPoint = new IPEndPoint(IPAddress.Any, port);
+
+        try
+        {
+            socket.Bind(endPoint);
+
+            if(OnCreateServerSuccess != null)
+            {
+                OnCreateServerSuccess("create success");
+            }
+        }
+        catch(Exception e)
+        {
+            Debug.Log(e.Message);
+
+            if(OnCreateServerFail != null)
+            {
+                OnCreateServerFail(e.Message);
+            }
+        }
+    }
+
+    public void CallbackCreateServerSuccess(string msg)
+    {
+        callback = Callback;
+
+        socket.BeginReceiveFrom(buffer, 0, byteSize, SocketFlags.None, ref endPoint, callback, socket);
+
+        connectionState = ConnectionState.Connect;
+
+        connectionRole = ConnectionRole.Server;
+    }
+
+    public void JoinServer(string ip, int port)
+    {
+        if (isJoiningRoom == true)
+            return;
+
+        isJoiningRoom = true;
+
+        try
+        {
+            IPAddress ipAddr = IPAddress.Parse(ip);
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            endPoint = new IPEndPoint(ipAddr, port);
+            socket.Connect(endPoint);
+        }
+        catch(Exception e)
+        {
+            if (OnJoinRoomFail != null)
+                OnJoinRoomFail(e.Message);
+
+            isJoiningRoom = false;
+            return;
+        }
+
+        socket.BeginReceiveFrom(buffer, 0, byteSize, SocketFlags.None, ref endPoint, callback, socket);
+
+        StartCoroutine(IEJoinRoomWaitCheckingConnnection());
+    }
+
+    private IEnumerator IEJoinRoomWaitCheckingConnnection()
+    {
+        yield return waitConnecting;
+
+        if(socket.Connected)
+        {
+            connectionState = ConnectionState.Connect;
+
+            connectionRole = ConnectionRole.Client;
+
+            if (OnJoinRoomSuccess != null)
+                OnJoinRoomSuccess("Join room success");
+        }
+        else
+        {
+            if (OnJoinRoomFail != null)
+                OnJoinRoomFail("Join room fail");
+        }
+
+        isJoiningRoom = false;
+    }
+
+    public void SendData(string dataStr)
+    {
+        if(connectionState == ConnectionState.Connect
+            && socket.Connected)
+        {
+            byte[] byteSend = Encoding.ASCII.GetBytes(dataStr);
+            socket.Send(byteSend);
+        }
+    }
+
+    public void Disconnect()
+    {
+        if(socket != null)
+            socket.Close();
+
+        connectionState = ConnectionState.Disconnect;
+        connectionRole = ConnectionRole.None;
+    }
+
+    private void OnDestroy()
+    {
+        Disconnect();
+        Debug.Log("Connection Close");
+    }
+
+    void Callback(IAsyncResult result)
+    {
+        if (result == null)
+            return;
+
+        Socket recvSocket = (Socket)result.AsyncState;
+        EndPoint clientEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+        if(connectionRole == ConnectionRole.Server &&
+            clientSocketList.Contains(recvSocket) == false)
+        {
+            clientSocketList.Add(recvSocket);
+        }
+
+        int msgLen = recvSocket.EndReceiveFrom(result, ref clientEndPoint);
+
+        byte[] localMsg = new byte[msgLen];
+        Array.Copy(buffer, localMsg, msgLen);
+        string msgStr = Encoding.ASCII.GetString(localMsg);
+
+        socket.BeginReceiveFrom(buffer, 0, byteSize, SocketFlags.None, ref endPoint, callback, socket);
+    }
+}
---
+++ GI471_Networking/Assets/Week1_Example/Scripts/PickupArray.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickupArray : MonoBehaviour
+{
+    public Text textDisplay;
+    public InputField inputSelectionText;
+    public Text textResult;
+    public Button enterFind;
+    
+    private List<string> textList = new List<string>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textList.Add("Unity");
+        textList.Add("Unreal");
+        textList.Add("ResidentEvil");
+        textList.Add("Google");
+        textList.Add("MongoDB");
+
+        enterFind.onClick.AddListener(OnEnterFind);
+
+        for(int i = 0; i < textList.Count; i++)
+        {
+            textDisplay.text += textList[i] + "\n";
+        }
+    }
+
+    void OnEnterFind()
+    {
+        bool isFoundText = false;
+
+        for(int i = 0; i < textList.Count; i++)
+        {
+            if(textList[i] == inputSelectionText.text)
+            {
+                isFoundText = true;
+                break;
+            }
+        }
+
+        if(isFoundText)
+        {
+            textResult.text = $"[ <color=green>{inputSelectionText.text}</color> ] is found.";
+        }
+        else
+        {
+            textResult.text = $"[ <color=red>{inputSelectionText.text}</color> ] is not found.";
+        }
+    }
+
+    
+}
