c87ff19520cbde3a872c3d01616c2c1bfa3e5aca
Kosit Taneakkhun <kost_t@bu.ac.th>
2021-01-30 00:07:06 +0700

    add websocket example

---
+++ GI471_Networking/Assets/ChatWebSocket/Scripts/UIManager.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+namespace ChatWebSocket
+{
+    public class UIManager : MonoBehaviour
+    {
+        public enum UIRootType
+        {
+            Connection,
+            Chat
+        }
+
+        public GameObject uiRootConnection;
+        public GameObject uiRootChat;
+        public GameObject uiRootPopUp;
+
+        public Button btnConnectToServer;
+        public Button btnPopupOK;
+        public Button btnSendMessage;
+
+        public InputField inputFieldIP;
+        public InputField inputFieldPort;
+        public InputField inputMessage;
+
+        public Text textPopUpMsg;
+        public Text textReceiveMsg;
+
+        private Dictionary<UIRootType, GameObject> uiRootDict = new Dictionary<UIRootType, GameObject>();
+
+        private WebSocketConnection webSocket;
+
+        private string receiveStr;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            webSocket = GetComponent<WebSocketConnection>();
+            btnConnectToServer.onClick.AddListener(BTN_ConnectToServer);
+            btnSendMessage.onClick.AddListener(BTN_SendMessage);
+            btnPopupOK.onClick.AddListener(BTN_PopupOK);
+
+            webSocket.OnConnectionSuccess += OnConnectionSuccess;
+            webSocket.OnConnectionFail += OnConnectionFail;
+            webSocket.OnReceive += OnReceiveMessage;
+
+            uiRootDict.Add(UIRootType.Connection, uiRootConnection);
+            uiRootDict.Add(UIRootType.Chat, uiRootChat);
+
+            OpenUIRoot(UIRootType.Connection);
+        }
+
+        private void Update()
+        {
+            if(receiveStr != textReceiveMsg.text)
+            {
+                textReceiveMsg.text = receiveStr;
+            }
+        }
+
+        void BTN_ConnectToServer()
+        {
+            webSocket.Connect(inputFieldIP.text, int.Parse(inputFieldPort.text));
+        }
+
+        void BTN_SendMessage()
+        {
+            if(!string.IsNullOrEmpty(inputMessage.text))
+                webSocket.Send(inputMessage.text);
+        }
+
+        void BTN_PopupOK()
+        {
+            uiRootPopUp.SetActive(false);
+        }
+
+        public void ShowPopup(string msg)
+        {
+            uiRootPopUp.SetActive(true);
+            textPopUpMsg.text = msg;
+        }
+
+        private void OpenUIRoot(UIRootType uiRootType)
+        {
+            foreach (var uiRoot in uiRootDict)
+            {
+                uiRoot.Value.SetActive(false);
+            }
+
+            uiRootDict[uiRootType].SetActive(true);
+        }
+
+        private void OnConnectionSuccess(string msg)
+        {
+            OpenUIRoot(UIRootType.Chat);
+        }
+
+        private void OnConnectionFail(string msg)
+        {
+            ShowPopup(msg);
+        }
+
+        private void OnReceiveMessage(string msg)
+        {
+            Debug.Log("OnReceive : " + msg);
+            receiveStr += msg + "\n";
+        }
+
+
+    }
+
+}
---
+++ GI471_Networking/Assets/ChatWebSocket/Scripts/WebSocketConnection.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WebSocketSharp;
+using System;
+
+namespace ChatWebSocket
+{
+    public class WebSocketConnection : MonoBehaviour
+    {
+
+        private WebSocket ws;
+
+        public delegate void DelegateHandler(string msg);
+
+        public event DelegateHandler OnConnectionSuccess;
+        public event DelegateHandler OnConnectionFail;
+        public event DelegateHandler OnReceive;
+
+        private bool isConnection;
+
+        public void Connect(string ip, int port)
+        {
+            if (isConnection)
+                return;
+
+            isConnection = true;
+
+            ws = new WebSocket($"ws://{ip}:{port}/socket.io/?EIO=3&transport=websocket");
+
+            ws.OnMessage += OnMessage;
+
+            ws.Connect();
+
+            StartCoroutine(WaitingConnectionState());
+        }
+
+        private IEnumerator WaitingConnectionState()
+        {
+            yield return new WaitForSeconds(1.0f);
+
+            if(ws.ReadyState == WebSocketState.Open)
+            {
+                if (OnConnectionSuccess != null)
+                    OnConnectionSuccess("Success");
+            }
+            else
+            {
+                if (OnConnectionFail != null)
+                    OnConnectionFail("Fail");
+            }
+
+            isConnection = false;
+        }
+
+        public void Disconnect()
+        {
+            if (ws != null)
+                ws.Close();
+        }
+
+        public bool IsConnected()
+        {
+            if (ws == null)
+                return false;
+
+            return ws.ReadyState == WebSocketState.Open;
+        }
+
+        public void Send(string data)
+        {
+            if (!IsConnected())
+                return;
+
+            ws.Send(data);
+        }
+
+        private void OnDestroy()
+        {
+            Debug.Log("OnDestroy");
+            if (ws != null)
+                ws.Close();
+        }
+
+        private void OnMessage(object sender, MessageEventArgs messageEventArgs)
+        {
+            if (OnReceive != null)
+                OnReceive(messageEventArgs.Data);
+        }
+    }
+}
+
+
